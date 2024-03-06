cc833e148378da61304ad0c3d5a31aa55b476bc3
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-03-06 18:49:24 +0530

    implemented

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground, setState } = props;
   const { background } = config;
+
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button
+      className={`${config?.classname}`}
+      onClick={() => {
+        setState([]);
+        selectNextBackground({ background: background });
+      }}
+    >
+      {config?.label ?? ""}
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,20 @@
+import React from "react";
 
+function Selection({ val, handleClicked, nextBackground, state }) {
+  console.log(val, nextBackground, "applyColor");
+  return (
+    <div
+      style={{
+        backgroundColor: `${
+          state.includes(val) ? nextBackground.background : ""
+        }`,
+      }}
+      onClick={() => handleClicked(val)}
+      className="selection"
+    >
+      {val}
+    </div>
+  );
+}
+
+export default Selection;
